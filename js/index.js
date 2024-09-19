6b30a5bbddc7093073be91a50b4b35571d6c33d2
ahmed mahmoud <ahmad.mahmoud1472003@gmail.com>
2024-09-19 17:59:37 +0300

    first commit

--- js/index.js
+++ js/index.js
@@ -26,22 +26,22 @@ function display(arr) {
   cartona = "";
 
   for (let i = 0; i < bookList.length; i++) {
-    cartona += `<tr>
+    cartona += `<tr>           
             <td>${1 + i}</td>
-            <td>${arr[i].name}</td>
-            <td><button onclick="openNewPage(${
-              arr[i].url
-            })" class="btn btn-visit btn-sm w-50">Visit <i class="fa-solid fa-eye pe-2 ms-3"></i></button></td>
+            <td>${bookList[i].name}</td>
+            <td><a href"${
+              bookList[i].url
+            }"><button class="btn btn-visit btn-sm w-50">Visit <i class="fa-solid fa-eye pe-2 ms-3"></i></button></a></td>
             <td><button onclick="deleteBook(${i})" class="btn btn-outline-danger btn-sm w-50">Delete <i class="fas fa-trash-alt ms-3"></i></button></td>
           </tr>`;
     console.log(i);
   }
   tbody.innerHTML = cartona;
 }
-function openNewPage(page) {
-  var x = window.open(`https://${page}`, "_blank");
-  console.log(x);
-}
+// function openNewPage(page) {
+//   var x = window.open(`https://${page}`, "_blank");
+//   console.log(x);
+// }
 function clear() {
   siteNameInput.value = null;
   siteURLInput.value = null;
